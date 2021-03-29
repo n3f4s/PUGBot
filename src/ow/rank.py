ada37fccc7b6f843bf63f49ac4f3cc309a9df2f6
n3f4s <tiv.crb@gmail.com>
2021-03-29 18:51:00 +0200

    Add OW-related types

---
+++ src/ow/rank.py
@@ -0,0 +1,19 @@
+"""
+Define helper class around SR
+"""
+
+from abc import ABC, abstractmethod
+
+class Rank(ABC):
+    @abstractmethod
+    def icon_url:
+        pass
+    @abstractmethod
+    def to_string:
+        pass
+    @abstractmethod
+    def min_sr:
+        pass
+    @abstractmethod
+    def max_sr:
+        pass
