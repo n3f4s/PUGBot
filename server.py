6bced47673cde48243021c7772c9495714d6813c
n3f4s <tiv.crb@gmail.com>
2021-03-30 08:57:32 +0200

    Stylecheck

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-from flask import Flask
-from flask import send_from_directory, render_template, Response, request
+""" Server """
+
+from queue import Queue
 import json
 import time
 import os
 
-from queue import Queue
+from flask import Flask
+from flask import send_from_directory, render_template, Response, request
+
 
 # https://maxhalford.github.io/blog/flask-sse-no-deps/
 class MessageBus:
@@ -22,13 +25,17 @@ class MessageBus:
                 self.listeners[i].put_nowait(msg)
             except:
                 del self.listeners[i]
-                
+
+
 # To get different escape characters
 class EscapedFlask(Flask):
     jinja_options = Flask.jinja_options.copy()
-    jinja_options.update(dict( variable_start_string='%%', variable_end_string='%%'))
+    jinja_options.update(dict(variable_start_string='%%',
+                              variable_end_string='%%'))
+
+
 app = EscapedFlask(__name__)
-           
+
 
 lobbyPlayers = [
     {
@@ -39,19 +46,19 @@ lobbyPlayers = [
             "tag": "A.tag",
             "overview": {
                 "tank": {
-                    "sr": 2586, 
+                    "sr": 2586,
                     "peakSr": 2856,
-                    "mostPlayed": [ "dva", "zarya", "winston" ]
+                    "mostPlayed": ["dva", "zarya", "winston"]
                 },
                 "damage": {
-                    "sr": 2500, 
+                    "sr": 2500,
                     "peakSr": 2900,
-                    "mostPlayed": [ "mcree", "ashe", "pharah" ]
+                    "mostPlayed": ["mcree", "ashe", "pharah"]
                 },
                 "support": {
-                   "sr": None, 
+                    "sr": None,
                     "peakSr": None,
-                    "mostPlayed": [ "lucio" ]
+                    "mostPlayed": ["lucio"]
                 },
             }
         }
@@ -64,19 +71,19 @@ lobbyPlayers = [
             "tag": "B.tag",
             "overview": {
                 "tank": {
-                    "sr": 2586, 
+                    "sr": 2586,
                     "peakSr": 2856,
-                    "mostPlayed": [ "dva", "zarya", "winston" ]
+                    "mostPlayed": ["dva", "zarya", "winston"]
                 },
                 "damage": {
-                    "sr": 2500, 
+                    "sr": 2500,
                     "peakSr": 2900,
-                    "mostPlayed": [ "mcree", "ashe", "pharah" ]
+                    "mostPlayed": ["mcree", "ashe", "pharah"]
                 },
                 "support": {
-                   "sr": None, 
+                    "sr": None,
                     "peakSr": None,
-                    "mostPlayed": [ "lucio" ]
+                    "mostPlayed": ["lucio"]
                 },
             }
         }
@@ -89,33 +96,36 @@ lobbyPlayers = [
             "tag": "C.tag",
             "overview": {
                 "tank": {
-                    "sr": 2586, 
+                    "sr": 2586,
                     "peakSr": 2856,
-                    "mostPlayed": [ "dva", "zarya", "winston" ]
+                    "mostPlayed": ["dva", "zarya", "winston"]
                 },
                 "damage": {
-                    "sr": 2500, 
+                    "sr": 2500,
                     "peakSr": 2900,
-                    "mostPlayed": [ "mcree", "ashe", "pharah" ]
+                    "mostPlayed": ["mcree", "ashe", "pharah"]
                 },
                 "support": {
-                   "sr": None, 
+                    "sr": None,
                     "peakSr": None,
-                    "mostPlayed": [ "lucio" ]
+                    "mostPlayed": ["lucio"]
                 },
             }
         }
     }
 ]
-           
+
+
 @app.route('/')
 def root():
-    #message_queue.put_nowait("event: new-client\ndata: {}\n\n")
+    # message_queue.put_nowait("event: new-client\ndata: {}\n\n")
     return render_template('index.html', lobbyPlayers=lobbyPlayers)
-    
+
+
 lobbyUpdates = MessageBus()
-    
-@app.route('/lobbyupdates', methods = ['GET'])
+
+
+@app.route('/lobbyupdates', methods=['GET'])
 def boradcastLobbyUpdates():
     def stream():
         listener = lobbyUpdates.listen()  # returns a queue.Queue
@@ -123,11 +133,12 @@ def boradcastLobbyUpdates():
             msg = listener.get()
             yield "data: {}\n\n".format(msg)
     return Response(stream(), mimetype="text/event-stream")
-    
+
+
 @app.route('/lobbyupdates', methods = ['POST'])
 def processLobbyUpdates():
     msg = request.data.decode("utf-8")
-    
+
     # Update lobby
     # TODO: make this not awful
     msg_json = json.loads(msg)
@@ -138,10 +149,11 @@ def processLobbyUpdates():
         movePlayer(msg_data["sourceID"], msg_data["targetGroup"])
     elif msg_type == "swap-player":
         swapPlayer(msg_data["sourceID"], msg_data["targetID"])
-    
+
     lobbyUpdates.broadcast(msg=msg)
     return ('', 200)
-    
+
+
 def findPlayer(playerId):
     for player in lobbyPlayers:
         if player['id'] == playerId:
@@ -149,35 +161,40 @@ def findPlayer(playerId):
     else:
         return None
 
+
 # This is kind of ewww...
 def movePlayer(playerId, targetGroup):
     findPlayer(playerId)['group'] = targetGroup
-    
+
+
 def swapPlayer(playerId, targetId):
     player1 = findPlayer(playerId)
     player2 = findPlayer(targetId)
     player1group = player1['group']
     player1['group'] = player2['group']
     player2['group'] = player1group
-        
-        
+
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-                               
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
+
+
 @app.route('/media/<path:path>')
 def send_js(path):
     return send_from_directory('media', path)
 
 
-    
 # Test stuff for testing
 @app.route('/sse-test')
 def ssetestpage():
     return render_template('sse_test.html')
-    
+
 counter = 0
+
+
 @app.route('/sse-testevents')
 def ssetestevents():
     def eventStream():
@@ -188,8 +205,8 @@ def ssetestevents():
             counter += 1
             yield "data: {}\n\n".format(counter)
     return Response(eventStream(), mimetype="text/event-stream")
-    
-    
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run(threaded=True)
