95eb99154280e1add1c77c0ef6da374740f97d9a
n3f4s <tiv.crb@gmail.com>
2021-03-30 21:20:38 +0200

    Basic handling of player joining lobby

--- src/bot.py
+++ src/bot.py
@@ -12,11 +12,12 @@ from btag import Btag
 
 # TODO:
 # 1- setup the service file in chocolytech with the appropriate auth
-# 2- split code in files
-# 3- have a basic that handle:
-#   - someone joining pugs for the first time
-#   - someone join pugs not for the first time
-# 4- github actions & secrets -> CI for testing and linting
+# 2- logging
+# 3- test
+# 4- Persistence
+# 5- integrate with backend
+# 6- Actual DB
+# 10- github actions & secrets -> CI for testing and linting
 
 CONFIG = {
     823930184067579954: GuildConfig(823930184067579954,
@@ -27,7 +28,19 @@ CONFIG = {
                                     "%")
  }
 
-players: Dict[int, List[Btag]] = {}
+
+class PUGPlayerStatus:
+    def __init__(self, member: discord.Member,
+                 lobby: discord.VoiceChannel,
+                 btags: List[Btag]):
+        self.member = member
+        self.lobby = lobby
+        self.btags = btags
+        self.is_registered = False
+
+
+def player_joined(player: discord.Member, tags: List[Btag], lobby: discord.VoiceChannel):
+    pass
 
 
 class MyClient(discord.Client):
@@ -35,37 +48,102 @@ class MyClient(discord.Client):
 
     logger = logging.getLogger('discord')
     logger.setLevel(logging.DEBUG)
+    players: Dict[int, PUGPlayerStatus] = {}
 
     async def on_ready(self):
         """Execute when client is ready"""
         print('Logged on as {0}!'.format(self.user))
 
+    def on_dm(self, message):
+        """
+        When receiving a DM:
+        - If the player isn't in `players` then do nothing (the player isn't in any discord lobby)
+        - Else:
+           - Add the btag
+           - If the player isn't registered the notify the backend
+        """
+        if message.author.id not in self.players:
+            await message.channel.send("You are not in any lobby, join a lobby")
+        else:
+            self.players[message.author.id].btags.append(Btag(message.content))
+            if not self.players[message.author.id].is_registered:
+                player_joined(self.players[message.author.id].member,
+                              self.players[message.author.id].btags)
+            await message.channel.send("{} is registered with {}"
+                                       .format(message.author.display_name,
+                                               ", ".join([e.to_string() for e in self.players[message.author.id].btags])))
+
     async def on_message(self, message):
         """Execute when received message"""
         if isinstance(message.channel, discord.DMChannel) and not message.author.bot:
-            if message.author.id not in players:
-                players[message.author.id] = []
-            players[message.author.id].append(Btag(message.content))
-            await message.channel.send("{} is registered with {}"
-                                       .format(message.author.display_name,
-                                               ", ".join([e.to_string() for e in players[message.author.id] ])))
+            self.on_dm(message)
 
+    def _come_from_team_vc(self, guild: int, before: int, after: int) -> bool:
+        """Test if the player is moving from a team VC to the corresponding lobby VC"""
+        for lobby in CONFIG[guild].lobbies:
+            if lobby.lobby == after and before in (lobby.team1, lobby.team2):
+                return True
+        return False
+
+    def _send_registration_dm(self, mem: discord.Member, after: discord.VoiceChannel):
+        self.players[mem.id] = PUGPlayerStatus(mem, after, [])
+        self.logger.info('Registering %s for lobby %s', mem.name, after.name)
+        dm_chan = await mem.create_dm()
+        await dm_chan.send("Give me your battle tag:")
+
+    # FIXME: don't assume that before and after guild are the same
+    def _handle_joining_lobby(self, mem: discord.Member,
+                              before: discord.VoiceState,
+                              after: discord.VoiceState):
+        """
+        Handle player joining a lobby
+        - If come from team VC from same lobby, do nothing
+        - If isn't in `players`, add the PUGPlayerStatus, send the DM
+        - If has status and is_registered, change lobby and notify
+        - If has status and not is_registered, re-send DM
+        """
+        before_id = before.channel.id
+        after_id = after.channel.id
+        guild_id = before.channel.guild.id
+        if self._come_from_team_vc(guild_id, before_id, after_id):
+            pass
+        elif mem.id not in self.players:
+            self._send_registration_dm(mem, after)
+        else:
+            if self.players[mem.id].is_registered:
+                self.players[mem.id].lobby = after.channel
+                player_joined(mem, self.players[mem.id].btags, after.channel)
+            else:
+                self._send_registration_dm(mem, after)
+
+    def _handle_leaving_lobby(self, mem: discord.Member,
+                              before: discord.VoiceState,
+                              after: discord.VoiceState):
+        """
+        Handle player leaving a lobby: remove the player form the statuses
+        """
+        del self.players[mem.id]
+
+    # FIXME:
+    # 1. If someone join, mark them as "logged in"
+    # 2. If someone join the wrong lobby first then join the right lobby, still ask the btag
+    # 3. If someone isn't registered, mark them as needed to be registered then do the registration from DM
+    # 4. If someone is registered, call the function to notify the server
     async def on_voice_state_update(self, mem: discord.Member,
                                     before: discord.VoiceState,
                                     after: discord.VoiceState):
-        """Callback for when someone change their VC state"""
-        # FIXME:
-        # 1. If someone join, mark them as "logged in"
-        # 2. If someone join the wrong lobby first then join the right lobby, still ask the btag
-        # 3. If someone isn't registered, mark them as needed to be registered then do the registration from DM
-        # 4. If someone is registered, call the function to notify the server
-        if not before.channel and after.channel and mem.id not in players:
+        """
+        Callback for when someone change their VC state
+        if someone join a lobby voice channel then call _handle_joining_lobby
+        """
+        if after.channel:
             is_lobby_vc = after.channel.id in [vc.lobby for vc in CONFIG[after.channel.guild.id].lobbies]
             if is_lobby_vc:
-                dm_chan = await mem.create_dm()
-                await dm_chan.send("Give me your battle tag:")
-                print("Registering {} for lobby {}"
-                      .format(mem.name, after.channel.name))
+                self._handle_joining_lobby(mem, before, after)
+        elif before.channel:
+            is_lobby_vc = before.channel.id in [vc.lobby for vc in CONFIG[after.channel.guild.id].lobbies]
+            if is_lobby_vc:
+                self._handle_leaving_lobby(mem, before, after)
 
 
 client = MyClient()
